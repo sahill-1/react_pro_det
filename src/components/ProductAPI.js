fd48a761897e0d1f0a0916aad659b4771e822658
sahill-1 <sv260397@gmail.com>
2023-10-16 12:46:41 +0530

    prodct page

---
+++ src/components/ProductAPI.js
@@ -0,0 +1,106 @@
+let products = [
+    {
+        "id": 1,
+        "name": "Example Laptop",
+        "category": "Electronics",
+        "brand": "TechCo",
+        "price": 899.99,
+        "currency": "USD",
+        "description": "A powerful and sleek laptop for all your computing needs.",
+        "specifications": {
+          "processor": "Intel Core i7",
+          "ram": "16GB DDR4",
+          "storage": "512GB SSD",
+          "display": "15.6-inch FHD",
+          "graphics": "NVIDIA GeForce GTX 1650",
+          "operatingSystem": "Windows 10 Home"
+        }
+      },
+      {
+        "id": 2,
+        "name": "Smartphone XYZ",
+        "category": "Electronics",
+        "brand": "MobileTech",
+        "price": 599.99,
+        "currency": "USD",
+        "description": "A feature-packed smartphone with a high-resolution camera and long-lasting battery life.",
+        "specifications": {
+          "display": "6.2-inch AMOLED",
+          "camera": "48MP main, 20MP front",
+          "storage": "128GB",
+          "processor": "Snapdragon 865",
+          "operatingSystem": "Android 10"
+        }
+      },
+      {
+        "id": 3,
+        "name": "Classic Leather Watch",
+        "category": "Fashion",
+        "brand": "StyleTime",
+        "price": 149.99,
+        "currency": "USD",
+        "description": "A stylish leather watch with a classic design, perfect for any occasion.",
+        "specifications": {
+          "material": "Genuine leather strap",
+          "case": "Stainless steel",
+          "waterResistance": "30 meters"
+        }
+      },
+      {
+        "id": 4,
+        "name": "Wireless Noise-Canceling Headphones",
+        "category": "Electronics",
+        "brand": "AudioTech",
+        "price": 249.99,
+        "currency": "USD",
+        "description": "Immerse yourself in music with these high-quality wireless headphones featuring noise-canceling technology.",
+        "specifications": {
+          "type": "Over-ear",
+          "noiseCancellation": "Active noise-canceling",
+          "batteryLife": "Up to 20 hours",
+          "wirelessRange": "30 feet"
+        }
+      },
+      {
+        "id": 5,
+        "name": "Ultra HD Smart TV",
+        "category": "Electronics",
+        "brand": "TechVision",
+        "price": 1299.99,
+        "currency": "USD",
+        "description": "Experience crystal-clear visuals and smart features with this Ultra HD Smart TV.",
+        "specifications": {
+          "screenSize": "55 inches",
+          "resolution": "4K Ultra HD",
+          "smartFeatures": "Built-in Wi-Fi, streaming apps, voice control"
+        }
+      },
+      {
+        "id": 6,
+        "name": "Fitness Tracker Watch",
+        "category": "Electronics",
+        "brand": "FitTech",
+        "price": 79.99,
+        "currency": "USD",
+        "description": "Track your fitness goals and stay connected with this advanced fitness tracker watch.",
+        "specifications": {
+          "activityTracking": "Steps, heart rate, sleep, calories",
+          "display": "Color touchscreen",
+          "waterResistance": "IP67"
+        }
+      },
+      {
+        "id": 7,
+        "name": "Designer Leather Handbag",
+        "category": "Fashion",
+        "brand": "ChicStyle",
+        "price": 199.99,
+        "currency": "USD",
+        "description": "Elevate your style with this luxurious designer leather handbag.",
+        "specifications": {
+          "material": "Genuine leather",
+          "size": "Medium",
+          "closure": "Zipper"
+        }
+      }
+];
